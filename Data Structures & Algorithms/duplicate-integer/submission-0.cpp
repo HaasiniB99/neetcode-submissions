d740a0156db8a16cfc6fe0c758591798f55014c1
HaasiniB99 <bhaasini6303@gmail.com>
2026-08-26 13:50:28 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        for(int i = 1 ; i < nums.size() ; i++){
+            if(nums[i] == nums[i-1]) return false;
+        }
+    }
+};
